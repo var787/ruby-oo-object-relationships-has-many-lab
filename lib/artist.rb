9cc371324ce06c12b651aebe21d65184599253de
var787 <hegde.rakshan@gmail.com>
2020-11-02 15:54:53 -0600

    commit tree 7525eb2fe22e416f31f87e4a2d8a6b00ab09c021

---
+++ lib/artist.rb
@@ -0,0 +1,18 @@
+class Artist
+    @@songs=[]
+   
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+        @songs = songs
+    end
+
+    def songs
+        @@songs
+    end
+
+
+
+
+end
